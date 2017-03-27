bedd5f03c15d039783db39ace459c6d5cc72b487
Adrian Luo <adrian.luo@goradii.com>
2017-03-27 10:01:28 -0400

    new test code

---
+++ Functions/JS-DOM-Effects/js/main.js
@@ -0,0 +1,15 @@
+var skillset = document.getElementsByClassName('skillset');
+//alert(skillset);
+
+function main() {
+	$('.skillset').hide();
+  $('.skillset').fadeIn(1000);
+  $('.projects').hide();
+  $('.projects-button').on('click', function() {
+  	$(this).next().slideToggle(400);
+    $(this).text('Projects Viewed');
+    $(this).toggleClass('active');
+  });
+}
+
+$(document).ready(main);
